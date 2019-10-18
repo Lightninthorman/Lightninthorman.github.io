f898cbb7443de1c3533c72d7dfb054b3e4316686
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2019-10-18 15:58:54 -0400

    still having ajax issues

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 
 
 $(() => {
-    const beerDb = $.ajax({
-        url: "https://sandbox-api.brewerydb.com/v2/styles/?key=810aa77f346d134a1c964135c4564018"
-    }).then(
-        (data)=>{
-            console.log(data[12]);
+// const db = $('<iframe />', { id: 'myFrame', src: 'https://sandbox-api.brewerydb.com/v2/beers/?key=810aa77f346d134a1c964135c4564018' }).appendTo('body');
+// console.log(db);
+$.ajax({
+        url: "1d282aedd7da4d3a9e029e7e74187ff7"
 
-        },
-        (error)=>{
-            alert('error')
-        })
+
+    }).then(
+        (data) =>{
+
+    },
+    (error) => {
+        alert('error')
+    })
 })
