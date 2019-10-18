5a6ad93ed128da9babf02b98637ba562ee9fe80d
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2019-10-18 17:22:05 -0400

    html and general css started

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ $(() => {
 // const db = $('<iframe />', { id: 'myFrame', src: 'https://sandbox-api.brewerydb.com/v2/beers/?key=810aa77f346d134a1c964135c4564018' }).appendTo('body');
 // console.log(db);
 $.ajax({
-        url: "1d282aedd7da4d3a9e029e7e74187ff7"
-
+        url: "https://swapi.co/api/starships/?name=Death Star"
 
     }).then(
         (data) =>{
-
+            console.log(data);
     },
     (error) => {
         alert('error')
