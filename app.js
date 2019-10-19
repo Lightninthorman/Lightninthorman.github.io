8f4ed8835d8e4df064c8d84fcf670ba2471f85d9
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2019-10-18 22:09:08 -0400

    beer button pulls up label and beer information including hop profile if provided

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 let searchInput = "";
 let searchBtn = "";
-
 let breweryId = "";
 
 $(() => {
 // const db = $('<iframe />', { id: 'myFrame', src: 'https://sandbox-api.brewerydb.com/v2/beers/?key=810aa77f346d134a1c964135c4564018' }).appendTo('body');
 // console.log(db);
-    const runDb = () => {
+    const beerDb = () => {
             $.ajax({
-                url: "https://sandbox-api.brewerydb.com/v2/" + searchBtn + "/" + "?key=810aa77f346d134a1c964135c4564018&name=" + searchInput
+                url: "https://sandbox-api.brewerydb.com/v2/beers/?key=810aa77f346d134a1c964135c4564018&withIngredients=y&name=" + searchInput + "*"
 
             }).then(
                 (data) =>{
-                    $('#col1').append($('<img>').attr('src',data.data[0].labels.contentAwareLarge))
+                    console.log(data.data);
+                    let $labelImg = $('<img>').attr('src',data.data[0].labels.contentAwareMedium)
+                    $('#col1').append($labelImg)
+                    let $resultsList = $('<ul>')
+                    let $beerName = $('<li>').html(data.data[0].name + '  |  abv. ' + data.data[0].abv + '%')
+                    let $style = $('<li>').html('Style: <br>' + data.data[0].style.name)
+                    let $description = $('<li>').html('Description: <br>' + data.data[0].description)
+                    let $ibu = $('<li>').html('IBU: ' + data.data[0].ibu)
+
+
+                    $('#col2').append($($resultsList).append($beerName).append($style).append($description).append($ibu))
+                    if(data.data[0].ingredients == undefined){
+                        $('#col2').append($('<p>').html("Hops: No hop profile provided"))
+                    }else {
+                        let hops = data.data[0].ingredients.hops
+                        let hopProfile = "Hops: "
+                        for (i = 0; i < hops.length; i++){
+                            hopProfile += hops[i].name + '  |  '
+                        }
+                    $('#col2').append($('<p>').html(hopProfile))
+                    }
+
 
             },
             (error) => {
@@ -21,10 +41,10 @@ $(() => {
     }
 
     $('#beerBtn').on('click', () => {
-        searchBtn='beers'
         searchInput = $('input').val()
         $('input').val('')
-        runDb()
+        $('.col').empty()
+        beerDb()
     })
 
 })
