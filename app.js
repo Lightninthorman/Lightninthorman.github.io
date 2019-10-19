22b50f43777d4b4c10b444ffee3aa6be4104e66d
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2019-10-18 20:55:50 -0400

    buttons are starting work. Figuring out the api a bit still too.

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
+let searchInput = "";
+let searchBtn = "";
 
+let breweryId = "";
 
 $(() => {
 // const db = $('<iframe />', { id: 'myFrame', src: 'https://sandbox-api.brewerydb.com/v2/beers/?key=810aa77f346d134a1c964135c4564018' }).appendTo('body');
 // console.log(db);
-$.ajax({
-        url: "https://swapi.co/api/starships/?name=Death Star"
+    const runDb = () => {
+            $.ajax({
+                url: "https://sandbox-api.brewerydb.com/v2/" + searchBtn + "/" + "?key=810aa77f346d134a1c964135c4564018&name=" + searchInput
 
-    }).then(
-        (data) =>{
-            console.log(data);
-    },
-    (error) => {
-        alert('error')
+            }).then(
+                (data) =>{
+                        console.log(data);
+                        for(let i = 0; i < data.data.length;i++)
+                        console.log(data.data[i].name);
+
+            },
+            (error) => {
+                alert('error')
+        })
+    }
+
+    $('#beerBtn').on('click', () => {
+        searchBtn='beers'
+        searchInput = $('input').val()
+        $(this).val('')
+        runDb()
     })
+
 })
